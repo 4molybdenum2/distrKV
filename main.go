075a08859294d8112ea532893dcfe59ae56dad2f
Tathagata Paul <tathagatapaul7@gmail.com>
2022-02-23 20:23:03 +0530

    added multiple shards and ways to redirect requests
    Signed-off-by: Tathagata Paul <tathagatapaul7@gmail.com>

--- main.go
+++ main.go
@@ -7,7 +7,9 @@ import (
 	"net/http"
 
 	db "github.com/4molybdenum2/distrKV/db"
+	"github.com/4molybdenum2/distrKV/pkg/config"
 	"github.com/4molybdenum2/distrKV/pkg/web"
+	"github.com/BurntSushi/toml"
 	"github.com/gorilla/mux"
 )
 
@@ -15,6 +17,8 @@ import (
 var (
 	path     = flag.String("loc", "", "Path to Bolt DB database")
 	httpAddr = flag.String("addr", "127.0.0.1:8080", "HTTP host endpoint")
+	cnf      = flag.String("config", "sharding.toml", "Config file for sharding data")
+	shard    = flag.String("shard", "", "Name of the shard for data")
 )
 
 func parseFlags() {
@@ -22,24 +26,45 @@ func parseFlags() {
 	if *path == "" {
 		log.Fatal("Must provide database location, ...in this case a file")
 	}
+	if *shard == "" {
+		log.Fatal("Must provide name of the shard")
+	}
 }
-
-var d *db.Database
-var closeFunc func() error
-var err error
 
 func main() {
 	fmt.Println("distrKV is a Distributed Key-Value Store")
 	parseFlags()
 
-	d, closeFunc, err = db.NewDatabase(*path)
+	// get toml data from file
+	var config config.Config
+	if _, err := toml.DecodeFile(*cnf, &config); err != nil {
+		log.Fatalf("DecodeFile(%q): %v", *cnf, err)
+	}
+
+	shardCount := len(config.Shards)
+	var shardIdx int = -1
+	var shardAddr = make(map[int]string)
+
+	for _, s := range config.Shards {
+		shardAddr[s.Idx] = s.Address
+		if s.Name == *shard {
+			shardIdx = s.Idx
+		}
+	}
+
+	if shardIdx < 0 {
+		log.Fatalf("Shard not found with name %q", *shard)
+	}
+	log.Printf("Shard count is %d and current shard is %d", shardCount, shardIdx)
+
+	d, closeFunc, err := db.NewDatabase(*path)
 	if err != nil {
 		log.Fatalf("New Database (%q) : %v", *path, err)
 	}
 	defer closeFunc()
 
 	// create new server
-	srv := web.NewServer(d)
+	srv := web.NewServer(d, shardCount, shardIdx, shardAddr)
 
 	// defined http router
 	r := mux.NewRouter()
@@ -47,5 +72,5 @@ func main() {
 	r.HandleFunc("/set", srv.SetKeyHandler)
 	http.Handle("/", r)
 
-	log.Fatal(http.ListenAndServe(":8080", r))
+	log.Fatal(http.ListenAndServe(*httpAddr, r))
 }
--- pkg/web/web.go
+++ pkg/web/web.go
@@ -2,35 +2,80 @@ package web
 
 import (
 	"fmt"
+	"hash/fnv"
+	"io"
 	"net/http"
 
 	"github.com/4molybdenum2/distrKV/db"
 )
 
 type Server struct {
-	db *db.Database
+	db         *db.Database
+	shardCount int
+	shardIdx   int
+	addr       map[int]string
 }
 
-func NewServer(db *db.Database) *Server {
+func NewServer(db *db.Database, shardCount int, shardIdx int, addr map[int]string) *Server {
 	return &Server{
-		db: db,
+		db:         db,
+		shardCount: shardCount,
+		shardIdx:   shardIdx,
+		addr:       addr,
 	}
 }
 
+func (s *Server) redirect(shardId int, w http.ResponseWriter, r *http.Request) {
+	url := "http://" + s.addr[shardId] + r.RequestURI
+	fmt.Fprintf(w, "redirecting from shard %d to shard %d (%q)\n", s.shardIdx, shardId, url)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		w.WriteHeader(500)
+		fmt.Fprintf(w, "Error redirecting the request: %v", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	io.Copy(w, resp.Body)
+}
+
+func (s *Server) getShard(key string) int {
+	h := fnv.New64()
+	h.Write([]byte(key))
+	shardIdx := int(h.Sum64() % uint64(s.shardCount))
+	return shardIdx
+}
+
 func (s *Server) GetKeyHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Get Endpoint hit!\n"))
 	r.ParseForm()
 	key := r.Form.Get("key")
+	// get shard
+	shardId := s.getShard(key)
 	value, err := s.db.GetKey(key)
-	fmt.Fprintf(w, "Value = %q, error = %v", value, err)
+
+	// if not current shard redirect request to necessary shard
+	if shardId != s.shardIdx {
+		s.redirect(shardId, w, r)
+		return
+	}
+	fmt.Fprintf(w, "Shard = %d, current shard = %d, addr = %q, Value = %q, error = %v", shardId, s.shardIdx, s.addr[shardId], value, err)
 }
 
 func (s *Server) SetKeyHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Set Endpoint hit!\n"))
 	r.ParseForm()
 	key := r.Form.Get("key")
 	value := r.Form.Get("value")
 
+	shardId := s.getShard(key)
+	// if calculated shard in which we want to set key is not the current shard then we need to
+	// make a set request on another address of the calculated shard
+	if shardId != s.shardIdx {
+		// redirect to necessary shard
+		s.redirect(shardId, w, r)
+		return
+	}
+
 	err := s.db.SetKey(key, []byte(value))
-	fmt.Fprintf(w, "Error = %v", err)
+	fmt.Fprintf(w, "Error = %v, shard= %d", err, shardId)
 }
